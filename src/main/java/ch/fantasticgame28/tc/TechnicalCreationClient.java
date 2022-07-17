ca426789d38b04bb346332dff1f995de61a34b39
Robin <rtmbaertschi007@gmail.com>
2022-07-17 16:22:18 +0200

    Update to 0.0.1.

--- src/main/java/ch/fantasticgame28/tc/TechnicalCreationClient.java
+++ src/main/java/ch/fantasticgame28/tc/TechnicalCreationClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 
 public class TechnicalCreationClient implements ClientModInitializer {
+    @SuppressWarnings("deprecation")
     @Override
     public void onInitializeClient() {
         ScreenRegistry.register(ModScreenHandler.SOLAR_PANEL_SCREEN_HANDLER, SolarPanelScreen::new);
